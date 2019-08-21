121c26a63c0971e217194fa0209d167a1d9b1790
Subhrajyoti Mondal <subhrajyoti.mondol@gmail.com>
2019-08-21 14:52:49 -0700

    more animation techniques
    learned more animation techniques, like translation and rotation

--- AnimationsPractice/app/src/main/java/com/example/animationspractice/MainActivity.java
+++ AnimationsPractice/app/src/main/java/com/example/animationspractice/MainActivity.java
@@ -10,25 +10,30 @@ public class MainActivity extends AppCompatActivity {
 
 //    public void fade2(View view){
 //        ImageView chimchar = (ImageView) findViewById(R.id.chimchar);
-//
 //        ImageView monferno = (ImageView) findViewById(R.id.monferno);
-//
-//
 //        chimchar.animate().alpha(0f).setDuration(2000);
-//
 //        monferno.animate().alpha(1f).setDuration(2000);
 //    }
 
     public void fade(View view){
         ImageView monferno = (ImageView) findViewById(R.id.monferno);
 
-        ImageView infernape = (ImageView) findViewById(R.id.infernape);
+        monferno.animate()
+                .scaleX(0.5f)
+                .scaleY(0.5f)
+                .rotation(1800f)
+                .setDuration(2000);
 
-            monferno.animate().alpha(0f).setDuration(2000);
+//        monferno.animate().rotation(1800f).setDuration(2000);
 
-            infernape.animate().alpha(1f).setDuration(2000);
+//      monferno.animate().translationXBy(10000f).setDuration(2000);
+//      monferno.animate().alpha(0f).setDuration(2000);
 
-//            Toast.makeText(getApplicationContext(),"Yo", Toast.LENGTH_LONG).show();
+
+//      ImageView infernape = (ImageView) findViewById(R.id.infernape);
+//      infernape.animate().alpha(1f).setDuration(2000);
+
+//       Toast.makeText(getApplicationContext(),"Yo", Toast.LENGTH_LONG).show();
 
     }
 
@@ -37,16 +42,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView chimchar = (ImageView) findViewById(R.id.chimchar);
-
-        ImageView monferno = (ImageView) findViewById(R.id.monferno);
-
-        chimchar.animate().alpha(0f).setDuration(2000);
-
-        monferno.animate().alpha(1f).setDuration(2000);
+//        ImageView monferno = (ImageView) findViewById(R.id.monferno);
+//        monferno.setTranslationX(-10000f);
+//        monferno.setTranslationY(-10000f);
 
 
-        //ImageView monferno = (ImageView) findViewById(R.id.monferno);
-        //monferno.setAlpha(0f);
+//        ImageView monferno = (ImageView) findViewById(R.id.monferno);
+//        monferno.setTranslationX(-10000f);
+
+
+//        ImageView chimchar = (ImageView) findViewById(R.id.chimchar);
+//        ImageView monferno = (ImageView) findViewById(R.id.monferno);
+//        chimchar.animate().alpha(0f).setDuration(2000);
+//        monferno.animate().alpha(1f).setDuration(2000);
+//        ImageView monferno = (ImageView) findViewById(R.id.monferno);
+//        monferno.setAlpha(0f);
     }
 }
