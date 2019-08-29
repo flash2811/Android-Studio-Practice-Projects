578dbdb760b35c2d06d0a73845dacc197f9d68d6
Subhrajyoti Mondal <subhrajyoti.mondol@gmail.com>
2019-08-29 15:01:54 -0700

    completed connect game
    fixed androidx version issue, added final touches

--- Connect3/app/src/main/java/com/example/connect3/MainActivity.java
+++ Connect3/app/src/main/java/com/example/connect3/MainActivity.java
@@ -2,28 +2,117 @@ package com.example.connect3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    int activePlayer = 0;
+    // 0 = yellow, 1 = red
 
-    public void dropIn(View view){
+    boolean gameIsActive = true;
 
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    // 2 means unplayed
+
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+    public void dropIn(View view) {
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationY(-1000f);
+        //counter.getTag().toString();
 
-        counter.setImageResource(R.drawable.yellow);
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.animate()
-                .translationYBy(1000f)
-                .rotation(360)
-                .setDuration(300)
-                .scaleXBy(0.4f)
-                .scaleYBy(0.4f)
-                .translationXBy(-40f);
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
+
+            gameState[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+
+            counter.animate()
+                    .translationYBy(1000f)
+                    .rotation(360)
+                    .setDuration(300);
+//                  .scaleXBy(0.4f)
+//                  .scaleYBy(0.4f)
+//                  .translationXBy(-40f);
+
+            for (int[] winningPositions : winningPositions) {
+                if (gameState[winningPositions[0]] == gameState[winningPositions[1]]
+                        && gameState[winningPositions[1]] == gameState[winningPositions[2]]
+                        && gameState[winningPositions[0]] != 2) {
+
+                    //someone has won
+
+                    gameIsActive = false;
+
+                    String winner = "Red";
+
+                    if (gameState[winningPositions[0]] == 0) {
+                        winner = "Yellow";
+                    }
+
+//                    System.out.println(gameState[winningPositions[0]]);
+//                    Toast.makeText(getApplicationContext(),"Win", Toast.LENGTH_LONG).show();
+
+
+                    TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+                    winnerMessage.setText(winner + " has won!");
+
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
+                } else {
+                    boolean gameIsOver = true;
+                    for (int counterState : gameState) {
+
+                        if (counterState == 2) gameIsOver = false;
+                    }
+
+                    if (gameIsOver) {
+                        TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+                        winnerMessage.setText("It's a draw");
+
+                        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                        layout.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        }
+
+        //counter.setImageResource(R.drawable.yellow);
+    }
+
+    public void playAgain(View view) {
+        gameIsActive = true;
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+
+        activePlayer = 0;
+
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+
+//        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+        androidx.gridlayout.widget.GridLayout gridLayout =
+                (androidx.gridlayout.widget.GridLayout) findViewById(R.id.gridLayout);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
+        }
     }
 
     @Override
